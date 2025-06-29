2538cfa979b9ac84ae09226e9cef0452d6b11ddb
imadityagolu <adityasng420.ak@gmail.com>
2025-06-29 18:13:42 +0530

    updated - contact me

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -78,7 +78,7 @@ function Home(){
     
         <li className="text-gray-600 hover:text-gray-900 active:text-gray-800 cursor-pointer transition-colors duration-300 ease-in-out">
         <a className="relative inline-block transition-all duration-300 after:absolute after:left-1/2 after:bottom-0 after:h-[2px] after:w-0 after:bg-current after:transition-all after:duration-300 after:origin-center hover:after:w-full after:-translate-x-1/2" href="#contact">
-        Contact</a>
+        Contact me</a>
         </li>
         </ul>
     
@@ -121,7 +121,7 @@ function Home(){
         Projects</a>
     
         <a className="block font-normal py-2 text-gray-900 hover:text-gray-600 text-center active:text-gray-800 transition-all duration-300 relative after:absolute after:left-1/2 after:bottom-0 after:h-[2px] after:w-0 after:bg-current after:transition-all after:duration-300 after:origin-center hover:after:w-full after:-translate-x-1/2" href="#contact">
-        Contact</a>
+        Contact me</a>
     
         <Link className="block cursor-pointer py-2 mt-4 text-center text-white bg-black rounded-lg text-[14px] hover:bg-gray-800 transition-all duration-300"to="/HireMe">Hire Me</Link>
     
